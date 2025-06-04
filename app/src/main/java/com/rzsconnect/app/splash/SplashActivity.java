3c5355997398a6c5c00325233bd922d9a6fde33f
tajim <tajimonthecloud@gmail.com>
2025-06-04 11:39:04 +0000

    feat: send to login activity after splash if not logged in

--- app/src/main/java/com/rzsconnect/app/splash/SplashActivity.java
+++ app/src/main/java/com/rzsconnect/app/splash/SplashActivity.java
@@ -1,23 +1,24 @@
 package com.rzsconnect.app.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import com.rzsconnect.app.MainActivity;
 import com.rzsconnect.app.R;
+import com.rzsconnect.app.authentication.LoginActivity;
 import com.rzsconnect.app.databinding.ActivitySplashBinding;
+import com.rzsconnect.app.utils.BaseActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
     private ActivitySplashBinding b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         initBinding();
         super.onCreate(savedInstanceState);
+        afterSplash(4000);
 
     }
 
@@ -26,4 +27,23 @@ public class SplashActivity extends AppCompatActivity {
         b = ActivitySplashBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
     }
+
+    private boolean isFirstLaunch(){
+        String name = getsSharedPreferences(getString(R.string.sfName));
+        return name == null;
+    }
+    private void afterSplash(int delayTime){
+
+        new Handler(Looper.getMainLooper()).postDelayed(()->{
+
+            if (isFirstLaunch())startActivity(new Intent(this, LoginActivity.class));
+            else startActivity(new Intent(this, MainActivity.class));
+            finish();
+
+        }, delayTime);
+
+    }
+
+
+
 }
