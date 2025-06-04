cd87487320da8cd9e27230ba8ed988022bc4a264
tajim <tajimonthecloud@gmail.com>
2025-06-04 11:28:01 +0000

    feat: add shared preference get and edit method in baseActivity

--- app/src/main/java/com/rzsconnect/app/utils/BaseActivity.java
+++ app/src/main/java/com/rzsconnect/app/utils/BaseActivity.java
@@ -1,7 +1,34 @@
 package com.rzsconnect.app.utils;
 
+import android.content.SharedPreferences;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.rzsconnect.app.R;
+
 public abstract class BaseActivity extends AppCompatActivity {
+
+    protected String getsSharedPreferences(String keyWord){
+        SharedPreferences sharedPreferences;
+        sharedPreferences = getSharedPreferences(getString(R.string.sharedPreferences), MODE_PRIVATE);
+
+        String data = sharedPreferences.getString(keyWord, null);
+
+        return data;
+
+
+    }
+
+    protected void editSharedPreferences(String keyWord, String value) {
+        SharedPreferences sharedPreferences;
+        sharedPreferences = getSharedPreferences(getString(R.string.sharedPreferences), MODE_PRIVATE);
+
+        SharedPreferences.Editor editor  = sharedPreferences.edit();
+        editor.putString(keyWord, value);
+        editor.apply();
+
+
+    }
+
 
 }
