c34affa3ecbc7da934e5efe9376f5e8453793328
tajim <tajimonthecloud@gmail.com>
2025-06-05 07:50:54 +0000

    feat: created new class for storing constant values, removed from strings.xml

---
+++ app/src/main/java/com/rzsconnect/app/utils/CONSTANTS.java
@@ -0,0 +1,21 @@
+package com.rzsconnect.app.utils;
+
+public class CONSTANTS {
+
+    public static final String DEFAULT_SHIFT = "Select Shift";
+    public static final String DEFAULT_CLASS = "Select Class";
+    public static final String DEFAULT_SECTION = "Select Section";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_NUMBER = "number";
+    public static final String KEY_STUDENT_ID = "student_id";
+    public static final String KEY_ROLL = "roll";
+    public static final String KEY_SHIFT = "shift";
+    public static final String KEY_CLASS = "class";
+    public static final String KEY_SECTION = "section";
+    public static final String KEY_PASSWORD = "password";
+    public static final String DOMAIN = "https://trtajim.xyz/apps/rzsConnect/";
+
+    public static final String STATUS_SUCCESS = "success";
+    public static final String STATUS_PENDING = "pending";
+    public static final String SHARED_PREFERENCE_NAME = "rzsConnect";
+}
